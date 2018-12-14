08bf1cf6b217e6f11e3653adeebef05938b2e467
Leote <fred_leote@hotmail.com>
2018-12-14 20:42:38 +0000

    Criada a tabela apresentações

---
+++ app/src/main/java/pt/ipg/seminarios/TabelaApresentacoes.java
@@ -0,0 +1,27 @@
+package pt.ipg.seminarios;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+public class TabelaApresentacoes implements BaseColumns {
+    private static final String NOME_TABELA = "apresentacoes" ;
+    public static final String CAMPO_SUMARIO = "sumario";
+    public static final String CAMPO_TITULO = "titulo";
+    public static final String CAMPO_ORADOR = "orador";
+    private SQLiteDatabase bd;
+
+    public TabelaApresentacoes(SQLiteDatabase bd) {
+        this.bd = bd;
+    }
+
+    public void cria(){
+        bd.execSQL("CREATE TABLE " + NOME_TABELA + " (" +
+                _ID +" INTENGER PRIMARY KEY AUTOINCREMENT," +
+                CAMPO_TITULO + " TEXT NOT NULL," +
+                CAMPO_ORADOR + " INTENGER NOT NULL," +
+                CAMPO_SUMARIO + " TEXT," +
+                "FOREIGN KEY (" + CAMPO_ORADOR + ") REFERENCES " +
+                TabelaOradores.NOME_TABELA + "(" + TabelaOradores._ID + ")" +
+                ")");
+    }
+}
